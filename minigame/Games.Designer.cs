89997f3da62fd8a05eba714a8b20b72e79225363
Vasilis Datseris <iekdatseris@gmail.com>
2021-06-15 15:02:40 +0300

    Changed Games Layout Changed Flight X Image

--- minigame/Games.Designer.cs
+++ minigame/Games.Designer.cs
@@ -39,6 +39,7 @@ namespace minigame
             this.ChessButton = new minigame.Details();
             this.SlotmachineButton = new minigame.Details();
             this.Flight_XButton = new minigame.Details();
+            this.Flight_XLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // SlotmachineLabel
@@ -62,7 +63,7 @@ namespace minigame
             this.ChessLabel.BackColor = System.Drawing.Color.Transparent;
             this.ChessLabel.Font = new System.Drawing.Font("Malgun Gothic", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ChessLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.ChessLabel.Location = new System.Drawing.Point(432, 504);
+            this.ChessLabel.Location = new System.Drawing.Point(105, 546);
             this.ChessLabel.Name = "ChessLabel";
             this.ChessLabel.Size = new System.Drawing.Size(149, 30);
             this.ChessLabel.TabIndex = 4;
@@ -76,7 +77,7 @@ namespace minigame
             this.BackgammonLabel.BackColor = System.Drawing.Color.Transparent;
             this.BackgammonLabel.Font = new System.Drawing.Font("Malgun Gothic", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BackgammonLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.BackgammonLabel.Location = new System.Drawing.Point(432, 741);
+            this.BackgammonLabel.Location = new System.Drawing.Point(432, 546);
             this.BackgammonLabel.Name = "BackgammonLabel";
             this.BackgammonLabel.Size = new System.Drawing.Size(157, 30);
             this.BackgammonLabel.TabIndex = 5;
@@ -89,7 +90,7 @@ namespace minigame
             this.ChessDevelopment.BackColor = System.Drawing.Color.Transparent;
             this.ChessDevelopment.Font = new System.Drawing.Font("Malgun Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ChessDevelopment.ForeColor = System.Drawing.Color.OrangeRed;
-            this.ChessDevelopment.Location = new System.Drawing.Point(89, 278);
+            this.ChessDevelopment.Location = new System.Drawing.Point(54, 576);
             this.ChessDevelopment.Name = "ChessDevelopment";
             this.ChessDevelopment.Size = new System.Drawing.Size(249, 274);
             this.ChessDevelopment.TabIndex = 6;
@@ -104,7 +105,7 @@ namespace minigame
             this.BackgammonDevelopment.BackColor = System.Drawing.Color.Transparent;
             this.BackgammonDevelopment.Font = new System.Drawing.Font("Malgun Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BackgammonDevelopment.ForeColor = System.Drawing.Color.OrangeRed;
-            this.BackgammonDevelopment.Location = new System.Drawing.Point(89, 515);
+            this.BackgammonDevelopment.Location = new System.Drawing.Point(380, 576);
             this.BackgammonDevelopment.Name = "BackgammonDevelopment";
             this.BackgammonDevelopment.Size = new System.Drawing.Size(249, 274);
             this.BackgammonDevelopment.TabIndex = 7;
@@ -120,7 +121,7 @@ namespace minigame
             this.BackgammonButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BackgammonButton.FlatAppearance.BorderSize = 0;
             this.BackgammonButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.BackgammonButton.Location = new System.Drawing.Point(404, 574);
+            this.BackgammonButton.Location = new System.Drawing.Point(404, 360);
             this.BackgammonButton.Name = "BackgammonButton";
             this.BackgammonButton.Size = new System.Drawing.Size(209, 164);
             this.BackgammonButton.TabIndex = 2;
@@ -135,7 +136,7 @@ namespace minigame
             this.ChessButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ChessButton.FlatAppearance.BorderSize = 0;
             this.ChessButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.ChessButton.Location = new System.Drawing.Point(404, 337);
+            this.ChessButton.Location = new System.Drawing.Point(94, 360);
             this.ChessButton.Name = "ChessButton";
             this.ChessButton.Size = new System.Drawing.Size(209, 164);
             this.ChessButton.TabIndex = 1;
@@ -161,7 +162,7 @@ namespace minigame
             // 
             this.Flight_XButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.Flight_XButton.BackColor = System.Drawing.Color.Transparent;
-            this.Flight_XButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Flight_XButton.BackgroundImage")));
+            this.Flight_XButton.BackgroundImage = global::minigame.Properties.Resources.Player;
             this.Flight_XButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.Flight_XButton.FlatAppearance.BorderSize = 0;
             this.Flight_XButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
@@ -172,12 +173,27 @@ namespace minigame
             this.Flight_XButton.UseVisualStyleBackColor = false;
             this.Flight_XButton.Click += new System.EventHandler(this.Flight_XButton_Click);
             // 
+            // Flight_XLabel
+            // 
+            this.Flight_XLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Flight_XLabel.AutoSize = true;
+            this.Flight_XLabel.BackColor = System.Drawing.Color.Transparent;
+            this.Flight_XLabel.Font = new System.Drawing.Font("Malgun Gothic", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Flight_XLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.Flight_XLabel.Location = new System.Drawing.Point(127, 278);
+            this.Flight_XLabel.Name = "Flight_XLabel";
+            this.Flight_XLabel.Size = new System.Drawing.Size(94, 30);
+            this.Flight_XLabel.TabIndex = 9;
+            this.Flight_XLabel.Text = "Flight X";
+            this.Flight_XLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Games
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(650, 900);
+            this.Controls.Add(this.Flight_XLabel);
             this.Controls.Add(this.Flight_XButton);
             this.Controls.Add(this.BackgammonDevelopment);
             this.Controls.Add(this.ChessDevelopment);
@@ -207,5 +223,6 @@ namespace minigame
         private System.Windows.Forms.Label ChessDevelopment;
         private System.Windows.Forms.Label BackgammonDevelopment;
         private Details Flight_XButton;
+        private System.Windows.Forms.Label Flight_XLabel;
     }
 }
